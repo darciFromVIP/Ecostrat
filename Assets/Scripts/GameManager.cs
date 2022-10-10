2023b4273a6865db31faf790d7594f64f697e87b
darciFromVIP <darcifromvip@gmail.com>
2022-10-10 15:14:06 +0200

    Implemented clickable bubbles on simple map

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,6 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
        
-        //Debug.Log(map.sprite.texture.GetPixel((int)targetPos.x * 4, (int)targetPos.y * 4).linear);    //ZISTENIE FARBY NA MAPE
     }
     private void Update()
     {
@@ -23,6 +22,12 @@ public class GameManager : MonoBehaviour
             Button bubbleInstance = Instantiate(bubble, map.transform);
             
             Vector2 targetPos = new Vector2(Random.Range(0, map.rectTransform.rect.width), Random.Range(0, map.rectTransform.rect.height));
+            Color color = map.sprite.texture.GetPixel((int)targetPos.x * 4, (int)targetPos.y * 4);
+            while (color.r >= 0.202 && color.r <= 0.206 && color.g >= 0.410 && color.g <= 0.414 && color.b >= 0.578 && color.b <= 0.582)
+            {
+                targetPos = new Vector2(Random.Range(0, map.rectTransform.rect.width), Random.Range(0, map.rectTransform.rect.height));
+                color = map.sprite.texture.GetPixel((int)targetPos.x * 4, (int)targetPos.y * 4);
+            }
             bubbleInstance.GetComponent<RectTransform>().anchoredPosition = targetPos;
         }
     }
