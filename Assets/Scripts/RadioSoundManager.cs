4739cdfcb67b60d5b1cbb7691db9eaf0f08b9bae
darciFromVIP <darcifromvip@gmail.com>
2023-03-30 14:22:38 +0200

    Final Touches

---
+++ Assets/Scripts/RadioSoundManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSoundManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public static RadioSoundManager instance;
+    public List<AudioClip> radioSounds;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        instance = this;
+    }
+    public void PlayRadioSound()
+    {
+        if (!audioSource.isPlaying)
+        {
+            int random = Random.Range(0, radioSounds.Count);
+            audioSource.PlayOneShot(radioSounds[random]);
+        }
+    }
+}
